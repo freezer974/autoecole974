9832097b0d379eb8d65e8a025bacdabe795d7a8d
freezer974 <tony_chevalier@hotmail.com>
2019-02-24 00:13:40 +0400

    correction bouton accueil

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require_once('header.php');
 ?>
             <h1>AutoEcole974, une conduite temporelle</h1>
-            <a href="utilisateurs/connexion_utilisateur.php" class="btn btn-primary mb-2">Connexion</a>            
+            <a href="#" class="btn btn-primary mb-2">Connexion</a>            
  <?php
     require_once('footer.php');
 ?>
